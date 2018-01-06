b75201f19e2069dae2c12de6104613a4bd35d0e2
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-01-07 00:44:01 +0300

    1.2/1.3/1.4 tasks added

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/tatyshev/sicp-on-clojure"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cljfmt "0.5.7"]])
--- test/sicp/first_chapter_test.clj
+++ test/sicp/first_chapter_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [sicp.first_chapter :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(testing "Exercise 1.2"
+  (is (= (exercise_1_2) -37/150)))
+
+(testing "Exercise 1.3"
+  (is (= (exercise_1_3 5 2 6) 61)))
+
+(testing "Exercise 1.4"
+  (is (= (exercise_1_4 1 3) 4))
+  (is (= (exercise_1_4 -1 3) 2)))
