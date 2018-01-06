c8fd1e7e7e5473bcebaa5dcbedad86a10e6cb48c
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-01-06 22:35:13 +0300

    update project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sicp-on-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "SICP practical exercises performed on clojure instead of scheme"
+  :url "https://github.com/tatyshev/sicp-on-clojure"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
